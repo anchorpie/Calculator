0a3ac298fd5d315d2f72360e4b02111f7595d322
anchorpie <57309437+anchorpie@users.noreply.github.com>
2021-10-11 11:36:15 +0500

    Initial commit

---
+++ CalculatorClientConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using CalulatorServer;
+using Grpc.Net.Client;
+using System;
+using System.Text.RegularExpressions;
+
+namespace CalculatorClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {               
+                string inputExpression = Console.ReadLine();
+
+                Console.WriteLine(ClientProcessing(inputExpression));
+            }
+        }
+
+        public static string ClientProcessing(string inputExpression)
+        {
+            switch (true)
+            {
+                case bool _ when Regex.IsMatch(inputExpression, @"[a-zA-Z]"):
+                    return "Expression contain letters, please check your input";
+                    
+                case bool _ when Regex.IsMatch(inputExpression, @"[а-яА-ЯёЁ]"):
+                    return "Выражение содержит буквы, пожалуйста проверьте ввод";
+
+                case bool _ when Regex.IsMatch(inputExpression, @"[!@#$%^&№:?_='|~`><]"):
+                    return "Expression contain special characters, please check your input";
+
+                case bool _ when Regex.IsMatch(inputExpression, @"[0-9]"):
+                    Console.WriteLine("Calculating ... ");
+                    return ($"Answer:  {ServerInteractions(inputExpression)}");
+
+                default:
+                    return "The expression contains special characters or cannot be validated, please check your input";
+            }
+        }
+
+        public static string ServerInteractions(string inputExpression)
+        {
+            var inputForServer = new Expression { ReceivedExpression = inputExpression };
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Calculator.CalculatorClient(channel);
+            var calculationResalt = client.GetExpressionResult(inputForServer);
+
+            return calculationResalt.Result;            
+        }
+    }
+}
---
+++ CalculatorServer/Services/CalculatorService.cs
@@ -0,0 +1,42 @@
+﻿using CalulatorServer;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace CalculatorServer.Services
+{
+    public static class MathCalculator
+    {
+        public static string Calc(Expression request)
+        {
+            try
+            {
+                double calculationResalt = Convert.ToDouble(new DataTable().Compute(request.ReceivedExpression, null));
+                return calculationResalt.ToString();
+            }
+            catch (Exception)
+            {
+                return "An error occurred while calculating an expression at the server level, please check your input";
+            }
+        }
+    }
+
+    class CalculatorService : Calculator.CalculatorBase
+    {
+        private readonly ILogger<CalculatorService> _logger;
+
+        public CalculatorService(ILogger<CalculatorService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CalculationResult> GetExpressionResult(Expression request, ServerCallContext context)
+        {
+            CalculationResult result = new CalculationResult();
+            result.Result = MathCalculator.Calc(request);
+            return Task.FromResult(result);
+        }
+    }
+}
